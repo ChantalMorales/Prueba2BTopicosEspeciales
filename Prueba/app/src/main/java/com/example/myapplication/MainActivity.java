5cbd9339756c2549956ef156ef04b0f67090687f
mastercode15 <sebastian.morales@epn.edu.ec>
2021-02-03 22:24:15 -0500

    configuración de autenticación de firebase

--- Prueba/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Prueba/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        email=(EditText)findViewById(R.id.email);
+        password=(EditText)findViewById(R.id.password);
     }
+
+    public void login(View view){
+        mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString());
+    }
+
 }
